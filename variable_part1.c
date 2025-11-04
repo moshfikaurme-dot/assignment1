7f15b29c1edfdd081a3f0cf60f40a557c7abd6ef
Moshfika Najnin <moshfikaurme@gmail.com>
2025-11-04 21:54:46 +0600

    added code

---
+++ variable_part1.c
@@ -0,0 +1,29 @@
+//soyabin tel = jalibag
+// tel = bottle
+//chal = polithin
+// mach = bag
+// numbers,
+//purno songkha, dosomik
+// alphabet
+//word
+
+// purnosongkha = int          -> %d
+//dosomik 3.1416 = float       -> %f
+//a, b, c, d = char            -> %c
+#include <stdio.h>
+int main(){
+
+int age = 25;
+// variable = a thing, which can vary
+// int = bottle er type
+// age = bottle er nam
+char favourite_alphabet = 'a';
+float cgpa = 3.7;
+// \n -> new line
+// printf("my age is %d", age);
+printf("my age is %d", age);
+printf("my favourite alphabet is %c", favourite_alphabet);
+printf("my CGPA IS %f", cgpa);
+
+}
+    
---
+++ variable_part2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+   // int number1 = 5;
+    //printf("%d\n", number1);
+
+   // number = 10;
+   // number2 = 15;
+   // printf("%d", number1);
+
+   //g = 9.8 (constant)
+   // float pi = 3.1416;// variable can be changed
+
+   float g =9.8;
+   // cont = contanat (dhrubok)
+   printf("the value of g is: %f\n", g);
+   //pi = 3.1416; // cant write it as it is a constant
+}
